46e6e99ac4be4ef7179c24027f9cfe889674280e
ammarShahab <ashahab007@gmail.com>
2026-03-03 14:19:02 +0600

    redirect user to home after logout

--- app/component/SignOutButton.jsx
+++ app/component/SignOutButton.jsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function SignOutButton() {
+  const router = useRouter();
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/" });
+    router.push(data.url);
+    router.refresh();
+  };
   return (
     <div>
-      <button className="font-bold" onClick={() => signOut()}>
+      <button
+        className="font-bold"
+        // onClick={() => signOut({ callbackUrl: "/" })}
+        onClick={handleSignOut}
+      >
         SignOut
       </button>
     </div>
