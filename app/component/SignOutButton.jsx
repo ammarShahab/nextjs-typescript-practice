be086caac4425afa143e95992d8cde20b51fff6a
Ashahab007 <ashahab007@gmail.com>
2026-03-07 03:28:14 +0600

    added image upload functionalities

--- app/component/SignOutButton.jsx
+++ app/component/SignOutButton.jsx
@@ -6,7 +6,8 @@ import { useRouter } from "next/navigation";
 export default function SignOutButton() {
   const router = useRouter();
   const handleSignOut = async () => {
-    const data = await signOut({ redirect: false, callbackUrl: "/" });
+    const data = await signOut({ callbackUrl: "/" });
+    // const data = await signOut({ redirect: false, callbackUrl: "/" });
     router.push(data.url);
     router.refresh();
   };
